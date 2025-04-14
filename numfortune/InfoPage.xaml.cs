b36be7dd41ac727d51076aae97f5905a8c797fed
Giulio Sorrentino <numerone@fastwebnet.it>
2025-04-14 19:24:26 +0200

    rotture di dazio

--- numfortune/InfoPage.xaml.cs
+++ numfortune/InfoPage.xaml.cs
@@ -2,13 +2,15 @@ namespace numfortune;
 
 public partial class InfoPage : ContentPage
 {
-	public InfoPage()
+	public static readonly Uri uri = new Uri("https://github.com/GiulianoSpaghetti/numfortune");
+
+    public InfoPage()
 	{
 		InitializeComponent();
 	}
     private async void OnInfo_Click(object sender, EventArgs e)
 	{
-		await Launcher.Default.OpenAsync(new Uri("https://github.com/GiulianoSpaghetti/numfortune"));
+		await Launcher.Default.OpenAsync(uri);
     }
 
 }
